0210cf645225b54d533899b0e93adc6dbf4bb0f1
Vladimir Jigulin <mogaika@yandex.ru>
2017-10-19 21:20:15 +0400

    Fix detection of second iso layer

--- tools/isoreplacer/isoreplacer.go
+++ tools/isoreplacer/isoreplacer.go
@@ -36,7 +36,7 @@ func FindFileSizePoses(iso *os.File, f *udf.File) map[int64]uint32 {
 	//f.FileEntry().AllocationDescriptors[0].Length
 	result := make(map[int64]uint32, 0)
 
-	fePos := udf.SECTOR_SIZE * (int64(f.FileEntryPosition) + int64(f.Udf.PartitionStart()))
+	fePos := udf.SECTOR_SIZE * (int64(f.GetFileEntryPosition()) + int64(f.Udf.PartitionStart()))
 	result[fePos+56] = 0
 
 	allocDescStart := uint32(fePos) + 176 + f.FileEntry().LengthOfExtendedAttributes
