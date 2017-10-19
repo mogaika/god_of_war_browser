37deb8fd78956879d44d2489efa99799478b1859
Vladimir Jigulin <mogaika@yandex.ru>
2017-10-19 21:20:15 +0400

    Fix detection of second iso layer

--- god_of_war_browser.go
+++ god_of_war_browser.go
@@ -30,7 +30,6 @@ import (
 
 func main() {
 	var addr, tocpath, dir, iso, part1name, part2name, tocname string
-	var secondLayerStart int64
 	flag.StringVar(&addr, "i", ":8000", "Address of server")
 	flag.StringVar(&tocpath, "toc", "", "Path to folder with toc file")
 	//flag.StringVar(&dir, "dir", "", "Path to unpacked wads and other stuff")
@@ -38,7 +37,6 @@ func main() {
 	flag.StringVar(&part1name, "partname1", "", "PART1.PAK name override")
 	flag.StringVar(&part2name, "partname2", "", "PART2.PAK name override")
 	flag.StringVar(&tocname, "tocname", "", "GODOFWAR.TOC name override")
-	flag.Int64Var(&secondLayerStart, "secondLayerStart", pack.IsoSecondLayerStart, "Start of second layer of DVD disk in iso. Use 0 if you dont want use second layer. Default value valid for 8 522 792 960 bytes iso")
 	flag.Parse()
 
 	var p pack.PackDriver
@@ -49,7 +47,7 @@ func main() {
 	toc.TocNameOverride(tocname)
 
 	if iso != "" {
-		p, err = pack.NewPackFromIso(iso, secondLayerStart)
+		p, err = pack.NewPackFromIso(iso)
 	} else if tocpath != "" {
 		p, err = pack.NewPackFromToc(tocpath)
 	} else if dir != "" {
