b3b34d25efca99c43f8c08f94f634c24a859c834
Vladimir Jigulin <mogaika@yandex.ru>
2018-06-14 20:34:52 +0400

    [wip] some work with toc stuff

--- drivers/toc/pak.go
+++ drivers/toc/pak.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/mogaika/god_of_war_browser/utils"
 	"github.com/mogaika/god_of_war_browser/vfs"
 )
 
 const (
-	PACK_ADDR_ABSOLUTE = iota // absolute offset to file relative to pak1
-	PACK_ADDR_INDEX           // size of previous paks + offset
+	PACK_ADDR_UNKNOWN  = iota
+	PACK_ADDR_ABSOLUTE // absolute offset to file relative to pak1
+	PACK_ADDR_INDEX    // size of previous paks + offset
 )
 
 type PaksArray struct {
@@ -52,10 +54,10 @@ func (pa *PaksArray) absoluteReadWriteAt(p []byte, off int64, doRead bool) (n in
 				processedN, err = pak.WriteAt(p[bufOff:bufOff+processAmount], off)
 			}
 			if err != nil {
-				return n, fmt.Errorf("[pak] absolute readwrite error: %v", err)
+				return n, fmt.Errorf("[pak] Absolute readwrite error: %v", err)
 			}
 			if int64(processedN) != processAmount {
-				return n, fmt.Errorf("[pak] absolute readwrite N calculation error: %v", err)
+				return n, fmt.Errorf("[pak] Absolute readwrite N calculation error: %v", err)
 			}
 			off += processAmount
 		}
@@ -71,10 +73,71 @@ func (pa *PaksArray) NewReaderWriter(e Encounter) *EncounterReaderWriter {
 	return &EncounterReaderWriter{pa: pa, e: e}
 }
 
-func (pa *PaksArray) Copy(from, to Encounter) error {
+func (pa *PaksArray) Move(from, to Encounter) error {
 	if from.Size != to.Size {
 		return fmt.Errorf("[pak] Wrong size amount %d != %d", from.Size, to.Size)
 	}
-	panic("Not implemented")
+
+	if from.Pak == to.Pak && from.Offset == to.Offset {
+		return nil
+	}
+
+	frw := pa.NewReaderWriter(from)
+	trw := pa.NewReaderWriter(to)
+
+	sizeInSectors := utils.GetRequiredSectorsCount(from.Size)
+	sizeInBytes := sizeInSectors * utils.SECTOR_SIZE
+
+	forwardCopy := true
+	bunchSize := int64(512)
+	if to.Pak == from.Pak {
+		// if we collide, then use memmove logic
+		if from.Offset < to.Offset+sizeInBytes && from.Offset+sizeInBytes >= to.Offset {
+			bunchSize = 1
+			if to.Offset > from.Offset {
+				forwardCopy = false
+			}
+		}
+	}
+
+	bigBuffer := make([]byte, bunchSize*utils.SECTOR_SIZE)
+
+	pos := int64(0)
+	if !forwardCopy {
+		pos += sizeInBytes
+	}
+
+	left := sizeInSectors
+	for left != int64(0) {
+		readSectorsAmount := left
+		if left > bunchSize {
+			left = bunchSize
+		}
+		readBytesAmount := readSectorsAmount * utils.SECTOR_SIZE
+
+		// if we do reverse copy, then pos pointed to upper bound
+		ioOffset := pos
+		if !forwardCopy {
+			ioOffset -= readBytesAmount
+		}
+
+		b := bigBuffer[:readBytesAmount]
+		if _, err := frw.ReadAt(b, ioOffset); err != nil {
+			return fmt.Errorf("[pak] Move() ReadAt (forwardCopy: %v, bunchSize: %v) error: %v",
+				forwardCopy, bunchSize, err)
+		}
+		if _, err := trw.WriteAt(b, ioOffset); err != nil {
+			return fmt.Errorf("[pak] Move() WriteAt (forwardCopy: %v, bunchSize: %v) error: %v",
+				forwardCopy, bunchSize, err)
+		}
+
+		if forwardCopy {
+			pos += readBytesAmount
+		} else {
+			pos -= readBytesAmount
+		}
+		left -= readSectorsAmount
+	}
+
 	return nil
 }
