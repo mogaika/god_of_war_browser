7881d19db3fd82770fb2bba3aae0e4fd30ccfb70
Vladimir Jigulin <mogaika@yandex.ru>
2018-06-18 20:17:18 +0400

    [wip] Remove pack driver interface

--- pack/pack.go
+++ pack/pack.go
@@ -10,12 +10,6 @@ import (
 	"github.com/mogaika/god_of_war_browser/vfs"
 )
 
-type PackDriver interface {
-	GetFileNamesList() []string
-	GetFile(fileName string) (vfs.File, error)
-	GetInstance(fileName string) (interface{}, error)
-}
-
 type FileLoader func(src utils.ResourceSource, r *io.SectionReader) (interface{}, error)
 
 var gHandlers map[string]FileLoader = make(map[string]FileLoader, 0)
@@ -36,7 +30,7 @@ func CallHandler(s utils.ResourceSource, r *io.SectionReader) (interface{}, erro
 
 type PackResSrc struct {
 	pf vfs.File
-	p  PackDriver
+	d  vfs.Directory
 }
 
 func (s *PackResSrc) Name() string {
@@ -47,15 +41,15 @@ func (s *PackResSrc) Size() int64 {
 	return s.pf.Size()
 }
 func (s *PackResSrc) Save(in *io.SectionReader) error {
-	if f, err := s.p.GetFile(s.pf.Name()); err != nil {
+	if f, err := vfs.DirectoryGetFile(s.d, s.pf.Name()); err != nil {
 		return fmt.Errorf("[pack] Cannot get file '%s': %v", s.pf.Name(), err)
 	} else {
 		return f.Copy(in)
 	}
 }
 
-func GetInstanceHandler(p PackDriver, fileName string) (interface{}, error) {
-	f, err := p.GetFile(fileName)
+func GetInstanceHandler(d vfs.Directory, fileName string) (interface{}, error) {
+	f, err := vfs.DirectoryGetFile(d, fileName)
 	if err != nil {
 		return nil, fmt.Errorf("[pack] Cannot get file '%s': %v", fileName, err)
 	}
@@ -66,7 +60,7 @@ func GetInstanceHandler(p PackDriver, fileName string) (interface{}, error) {
 	}
 	defer f.Close()
 
-	inst, err := CallHandler(&PackResSrc{p: p, pf: f}, r)
+	inst, err := CallHandler(&PackResSrc{d: d, pf: f}, r)
 	if err != nil {
 		return nil, fmt.Errorf("Handler error: %v", err)
 	}
